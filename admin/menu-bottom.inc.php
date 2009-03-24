460e67c1f9f54ace5dea1dacd968a302b3124496
Andrey Nikitin <avreg-support@NO_SPAM.mail.ru>
2009-03-24 12:14:32 +0300

    add subscribe to maillist link

--- admin/menu-bottom.inc.php
+++ admin/menu-bottom.inc.php
@@ -1,5 +1,7 @@
 <?php
 print '<div align="left">'."\n";
 print '<div class="menu1"><a href="'.$conf['prefix'].'/admin/bugreport.php" target="content">'.$left_bug.'</a></div>'."\n";
-print '<div class="menu1"><a href="http://avreg.net/" target="_blank">'.$DevelopersSite.'</a></div>'."\n";
+print '<div class="menu1"><a href="http://avreg.net/" target="_blank">'.$SubscribeUrl.'</a></div>'."\n";
+print '</div>'."\n";
+print '<div class="menu1"><a href="http://avreg.net/maillist.html" target="_blank">'.$DevelopersSite.'</a></div>'."\n";
 print '</div>'."\n";
