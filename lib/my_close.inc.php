8d4ec65a8726ba08c66343780548764053912039
Andrey Nikitin <avreg-support@mail.ru>
2010-03-04 14:06:04 +0300

    bugfix: mysql result do not freed

--- lib/my_close.inc.php
+++ lib/my_close.inc.php
@@ -1,9 +1,14 @@
 <?php
-     /* Free resultset */
-    if ( !isset($result) && !is_null($result) ) 
-         mysql_free_result($result);
 
-    /* Closing connection */
-    if (!isset($link) && !is_null($link)) 
-         mysql_close($link);
+/* Free resultset */
+if ( isset($result) && !is_null($result) )  {
+   mysql_free_result($result);
+   unset($result);
+}
+
+/* Closing connection */
+if ( isset($link) && !is_null($link) ) {
+   mysql_close($link);
+   unset($link);
+}
 ?>
