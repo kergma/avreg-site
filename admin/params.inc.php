3d769462463ccf456bd6ddb13a26e41456d50023
Andrey Nikitin <avreg-support@mail.ru>
2010-07-02 13:02:38 +0400

    s/save_(video|audio)/record_\1/

--- admin/params.inc.php
+++ admin/params.inc.php
@@ -2,55 +2,55 @@
 
 function translit_ru($rustr)
 {
-  if (empty($rustr))
+   if (empty($rustr))
       return $rustr;
-  mb_regex_encoding('UTF-8');
+   mb_regex_encoding('UTF-8');
 
-  $patterns = array(
-  'а','б','в','г','д','е','ё','з','и','й','к','л','м',
-  'н','о','п','р','с','т','у','ф','х','ъ','ы','э',
-  'А','Б','В','Г','Д','Е','Ё','З','И','Й','К','Л','М',
-  'Н','О','П','Р','С','Т','У','Ф','Х','Ъ','Ы','Э',
-  'ж','ц','ч','ш','щ','ь','ю','я',
-  'Ж','Ц','Ч','Ш','Щ','Ь','Ю','Я');
+   $patterns = array(
+      'а','б','в','г','д','е','ё','з','и','й','к','л','м',
+      'н','о','п','р','с','т','у','ф','х','ъ','ы','э',
+      'А','Б','В','Г','Д','Е','Ё','З','И','Й','К','Л','М',
+      'Н','О','П','Р','С','Т','У','Ф','Х','Ъ','Ы','Э',
+      'ж','ц','ч','ш','щ','ь','ю','я',
+      'Ж','Ц','Ч','Ш','Щ','Ь','Ю','Я');
 
-  $replacements = array(
-  'a','b','v','g','d','e','e','z','i','y','k','l','m',
-  'n','o','p','r','s','t','u','f','h','`','i','e',
-  'A','B','V','G','D','E','E','Z','I','Y','K','L','M',
-  'N','O','P','R','S','T','U','F','H','`','I','E',
-  'zh', 'ts', 'ch', 'sh', 'shch', '', 'yu', 'ya',
-  'ZH', 'TS', 'CH', 'SH', 'SHCH', '', 'YU', 'YA');
+   $replacements = array(
+      'a','b','v','g','d','e','e','z','i','y','k','l','m',
+      'n','o','p','r','s','t','u','f','h','`','i','e',
+      'A','B','V','G','D','E','E','Z','I','Y','K','L','M',
+      'N','O','P','R','S','T','U','F','H','`','I','E',
+      'zh', 'ts', 'ch', 'sh', 'shch', '', 'yu', 'ya',
+      'ZH', 'TS', 'CH', 'SH', 'SHCH', '', 'YU', 'YA');
 
-  for ($i=0; $i<sizeof($patterns); $i++) {
-        $rustr = mb_ereg_replace($patterns[$i], $replacements[$i], $rustr);
-  }
+   for ($i=0; $i<sizeof($patterns); $i++) {
+      $rustr = mb_ereg_replace($patterns[$i], $replacements[$i], $rustr);
+   }
 
-  return preg_replace(array('/[^A-Za-z0-9\-_\.~]/u'), array('_'), $rustr);
+   return preg_replace(array('/[^A-Za-z0-9\-_\.~]/u'), array('_'), $rustr);
 }
 
 
 function WhatViddev ( $dev_file )
 {
-      $ret = -1;
+   $ret = -1;
 
-      if ( is_readable($dev_file) )
-      {
-               $handle = fopen ($dev_file, 'r');
-               while (!feof ($handle)) {
-                        $buffer = fgets($handle, 256);
-                        if ( preg_match ('/^type +: VID_TYPE_CAPTURE.+/', $buffer))
-                        {
-                              $ret = 1;
-                              break;
-                        }	elseif 	( preg_match ('/^name +: Video loopback \d* input$/', $buffer)) {
-                              $ret = 2;
-                              break;
-                        }
-               }
-               fclose ($handle);
+   if ( is_readable($dev_file) )
+   {
+      $handle = fopen ($dev_file, 'r');
+      while (!feof ($handle)) {
+         $buffer = fgets($handle, 256);
+         if ( preg_match ('/^type +: VID_TYPE_CAPTURE.+/', $buffer))
+         {
+            $ret = 1;
+            break;
+         }	elseif 	( preg_match ('/^name +: Video loopback \d* input$/', $buffer)) {
+            $ret = 2;
+            break;
+         }
       }
-      return $ret;
+      fclose ($handle);
+   }
+   return $ret;
 }
 
 function find_param_defs($par_name) {
@@ -77,14 +77,14 @@ function CheckParVal($_param, $_value)
    } else {
       switch ( $par_defs['type'] )
       {
-         case $GLOBALS['INT_VAL']:
-            if ( !is_numeric($_value) )
-               $ret = FALSE;
-            break;
-         case $GLOBALS['BOOL_VAL']:
-            if ( !($_value == '0' || $_value == '1'))
-               $ret = FALSE;
-            break;
+      case $GLOBALS['INT_VAL']:
+         if ( !is_numeric($_value) )
+            $ret = FALSE;
+         break;
+      case $GLOBALS['BOOL_VAL']:
+         if ( !($_value == '0' || $_value == '1'))
+            $ret = FALSE;
+         break;
       }
    }
    if ( !$ret ) 
@@ -95,141 +95,133 @@ function CheckParVal($_param, $_value)
 
 function checkParam ( $parname, $parval )
 {
-switch ( $parname )
-{
-      case 'v4l_dev':
-      case 'v4l_pipe':
+   switch ( $parname )
+   {
+   case 'v4l_dev':
+   case 'v4l_pipe':
       if ($parname == 'v4l_dev' ) $dev_code = 1; else $dev_code = 2;
-               $viddev_nums = array();
-               if (is_dir('/proc/video/dev'))
-               {
-               /* 2.4 kernel = LinuxDVRv4.x */
-                        if ($dh = opendir('/proc/video/dev'))
-                        {
-                              while (($file = readdir($dh)) !== false) {
-                                       if ( preg_match ('/^video(\d{1,2})$/', $file, $matches)) {
-                                       // print "A match was found.";
-                                                if ( $dev_code == WhatViddev('/proc/video/dev/'.$file) )
-                                                     $viddev_nums[] = $matches[1];
-                                       }
-                              }
-                              closedir($dh);
-                        }
-                        sort($viddev_nums, SORT_NUMERIC);
-               } else {
-                   /* 2.6 kernel */
-                   $all_v4l_devs = glob('/dev/video[0-9]*');
-                   if ( FALSE === $all_v4l_devs ) {
-                       $ret = '<p style="color:'.$GLOBALS['error_color'].';">'. $GLOBALS['notVidDevs'] .'</p>'."\n";
-                       break; 
-                   }
-
-                   $all_v4l_devs_nrs = array();
-                   foreach ($all_v4l_devs as $file) {
-                       if ( preg_match ('/^\/dev\/video(\d+)$/', $file, $matches))
-                           $all_v4l_devs_nrs[] =  (int) $matches[1];
-                   }
-                   sort($all_v4l_devs_nrs, SORT_NUMERIC);
-
-                   if ( isset($GLOBALS['conf']['v4loop-dev-offset']) ) 
-                       $_v4loop_dev_offset = (int)$GLOBALS['conf']['v4loop-dev-offset'];
-                   else {
-                       $ret = '<p style="color:'.$GLOBALS['error_color'].';">not defined "v4loop-dev-offset"</p>'."\n";
-                       break;
-                   }
-                   $c=0;
-                   foreach ($all_v4l_devs_nrs as $_dev_nr) {
-                       if ( $dev_code === 1  /* v4l capturing dev */ ) {
-                           if ( $_dev_nr < $_v4loop_dev_offset)
-                               $viddev_nums[] = $_dev_nr;
-                       } else /* v4l pipes */ {
-                          if ( $_dev_nr >= $_v4loop_dev_offset ) {
-                             $c++;
-                             if ( $c % 2 /*через одного */ )
-                                $viddev_nums[] = $_dev_nr;
-                          }
-                       }
-                   }
-
-               } /* 2.6 kernel */
-
-               if ( count($viddev_nums) > 0 ) {
-                   $ret = getSelectHtmlByName('fields['.$parname.']', $viddev_nums, FALSE, 1, 0, $parval, TRUE, FALSE,
-                       '/dev/video');
-               } else 
-                   $ret = '<p style="color:"'.$GLOBALS['error_color'].';">'. $GLOBALS['notVidDevs'] .'</p>'."\n";
-               break;
-      case 'norm':
-               if ( $parval == '' || is_null($parval))
-                        $sel = '';
-               else
-                        $sel = $GLOBALS['vid_standarts'][$parval];
-               $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['vid_standarts'], FALSE, 1, 0, $sel, TRUE, FALSE);
-               break;
-      case 'deinterlacer':
-               if ( $parval == '' )
-                        $sel = '';
-               else
-                        $sel = $GLOBALS['deinterlacers'][$parval];
-               $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['deinterlacers'], FALSE, 1, 0, $sel, TRUE, FALSE);
-               break;
-      case 'mask_file':
-               if (empty($parval) )
-                        $ret = $GLOBALS['strEmpted'] . '<br>';
-               else {
-                        $ret  = '<a href="'.$GLOBALS['conf']['prefix'].'/masks/'.basename($parval).'"  target="_blank">'.basename($parval).'</a><br>'."\n";
-                        $ret .= $GLOBALS['strDelete'].' &nbsp;&nbsp;<input type="checkbox" name="'.$parname.'_del"><br>'."\n";
+      $viddev_nums = array();
+      if (is_dir('/proc/video/dev'))
+      {
+         /* 2.4 kernel = LinuxDVRv4.x */
+         if ($dh = opendir('/proc/video/dev'))
+         {
+            while (($file = readdir($dh)) !== false) {
+               if ( preg_match ('/^video(\d{1,2})$/', $file, $matches)) {
+                  // print "A match was found.";
+                  if ( $dev_code == WhatViddev('/proc/video/dev/'.$file) )
+                     $viddev_nums[] = $matches[1];
                }
-               $ret .= '<input type="hidden" name="MAX_FILE_SIZE" value="500000">'."\n";
-               $ret .= '<input type="file" name="'.$parname.'" size=20 maxlength=200>'."\n";
-               break;
-      case 'cam_type':
-               $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['strCamType'], FALSE, 1, 0, $parval, TRUE, FALSE);
+            }
+            closedir($dh);
+         }
+         sort($viddev_nums, SORT_NUMERIC);
+      } else {
+         /* 2.6 kernel */
+         $all_v4l_devs = glob('/dev/video[0-9]*');
+         if ( FALSE === $all_v4l_devs ) {
+            $ret = '<p style="color:'.$GLOBALS['error_color'].';">'. $GLOBALS['notVidDevs'] .'</p>'."\n";
+            break; 
+         }
+
+         $all_v4l_devs_nrs = array();
+         foreach ($all_v4l_devs as $file) {
+            if ( preg_match ('/^\/dev\/video(\d+)$/', $file, $matches))
+               $all_v4l_devs_nrs[] =  (int) $matches[1];
+         }
+         sort($all_v4l_devs_nrs, SORT_NUMERIC);
+
+         if ( isset($GLOBALS['conf']['v4loop-dev-offset']) ) 
+            $_v4loop_dev_offset = (int)$GLOBALS['conf']['v4loop-dev-offset'];
+         else {
+            $ret = '<p style="color:'.$GLOBALS['error_color'].';">not defined "v4loop-dev-offset"</p>'."\n";
+            break;
+         }
+         $c=0;
+         foreach ($all_v4l_devs_nrs as $_dev_nr) {
+            if ( $dev_code === 1  /* v4l capturing dev */ ) {
+               if ( $_dev_nr < $_v4loop_dev_offset)
+                  $viddev_nums[] = $_dev_nr;
+            } else /* v4l pipes */ {
+               if ( $_dev_nr >= $_v4loop_dev_offset ) {
+                  $c++;
+                  if ( $c % 2 /*через одного */ )
+                     $viddev_nums[] = $_dev_nr;
+               }
+            }
+         }
+
+      } /* 2.6 kernel */
+
+      if ( count($viddev_nums) > 0 ) {
+         $ret = getSelectHtmlByName('fields['.$parname.']', $viddev_nums, FALSE, 1, 0, $parval, TRUE, FALSE,
+            '/dev/video');
+      } else 
+         $ret = '<p style="color:"'.$GLOBALS['error_color'].';">'. $GLOBALS['notVidDevs'] .'</p>'."\n";
       break;
-      case 'InetCam_Proto':
-               $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['strNetProto'], FALSE, 1, 0, $parval, TRUE, FALSE);
+   case 'norm':
+      if ( $parval == '' || is_null($parval))
+         $sel = '';
+      else
+         $sel = $GLOBALS['vid_standarts'][$parval];
+      $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['vid_standarts'], FALSE, 1, 0, $sel, TRUE, FALSE);
+      break;
+   case 'deinterlacer':
+      if ( $parval == '' )
+         $sel = '';
+      else
+         $sel = $GLOBALS['deinterlacers'][$parval];
+      $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['deinterlacers'], FALSE, 1, 0, $sel, TRUE, FALSE);
+      break;
+   case 'mask_file':
+      if (empty($parval) )
+         $ret = $GLOBALS['strEmpted'] . '<br>';
+      else {
+         $ret  = '<a href="'.$GLOBALS['conf']['prefix'].'/masks/'.basename($parval).'"  target="_blank">'.basename($parval).'</a><br>'."\n";
+         $ret .= $GLOBALS['strDelete'].' &nbsp;&nbsp;<input type="checkbox" name="'.$parname.'_del"><br>'."\n";
+      }
+      $ret .= '<input type="hidden" name="MAX_FILE_SIZE" value="500000">'."\n";
+      $ret .= '<input type="file" name="'.$parname.'" size=20 maxlength=200>'."\n";
+      break;
+   case 'cam_type':
+      $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['strCamType'], FALSE, 1, 0, $parval, TRUE, FALSE);
+      break;
+   case 'InetCam_Proto':
+      $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['strNetProto'], FALSE, 1, 0, $parval, TRUE, FALSE);
       break; 
 
-      case 'V.save_fmt':
-               $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['strFileFmt'], FALSE, 1, 0, $parval, TRUE, FALSE);
+   case 'V.save_fmt':
+      $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['strFileFmt'], FALSE, 1, 0, $parval, TRUE, FALSE);
       break;
    case 'input':
       $ret = getSelectHtml('fields['.$parname.']', array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15), FALSE, 1, 0, $parval, TRUE, FALSE);
       break;
-      case 'save_video':
-           if ( $parval == '' || is_null($parval) )
-              $sel = '';
-           else
-              $sel = $GLOBALS['save_video_modes'][$parval];
-           $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['save_video_modes'], FALSE, 1, 0, $sel, TRUE, FALSE);
-               break;
-      case 'A.force_fmt':
-               $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['str_audio_force_fmt'], FALSE, 1, 0, $parval, TRUE, FALSE);
-               break;
+   case 'A.force_fmt':
+      $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['str_audio_force_fmt'], FALSE, 1, 0, $parval, TRUE, FALSE);
+      break;
+   case 'A.save_fmt':
+      $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['str_audio_save_fmt'], FALSE, 1, 0, $parval, TRUE, FALSE);
+      break;
 
-      case 'A.save_fmt':
-               $ret = getSelectHtmlByName('fields['.$parname.']', $GLOBALS['str_audio_save_fmt'], FALSE, 1, 0, $parval, TRUE, FALSE);
-               break;
-
-      case 'rotate':
-               if ( $parval == '' || is_null($parval) || $parval == '0' )
-                        $sel = '';
-               else
-                        $sel = $GLOBALS['flip_type'][$parval-1];
-               $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['flip_type'], FALSE, 1, 1, $sel, TRUE, FALSE);
-               break;
-      case 'v4l_hack':
-               if ( $parval == '' || is_null($parval) || $parval == '0' )
-                        $sel = '';
-               else
-                        $sel = $GLOBALS['v4l_hacks'][$parval-1];
-               $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['v4l_hacks'], FALSE, 1, 1, $sel, TRUE, FALSE);
-               break;
-      default:
+   case 'rotate':
+      if ( $parval == '' || is_null($parval) || $parval == '0' )
+         $sel = '';
+      else
+         $sel = $GLOBALS['flip_type'][$parval-1];
+      $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['flip_type'], FALSE, 1, 1, $sel, TRUE, FALSE);
+      break;
+   case 'v4l_hack':
+      if ( $parval == '' || is_null($parval) || $parval == '0' )
+         $sel = '';
+      else
+         $sel = $GLOBALS['v4l_hacks'][$parval-1];
+      $ret = getSelectHtml('fields['.$parname.']', $GLOBALS['v4l_hacks'], FALSE, 1, 1, $sel, TRUE, FALSE);
+      break;
+   default:
       $ret = '<p style="color: '.$GLOBALS['error_color'].'">'. sprintf($GLOBALS['unknownCheckParams'], $parname) .'</p>'."\n";
-} // switch
+   } // switch
 
-return $ret;
+   return $ret;
 }
 
 /* CorrectParVal($parname, &$parval) */
@@ -238,8 +230,8 @@ function CorrectParVal($parname, $parval)
    return; /* disable function */
    switch ( $parname )
    {
-      case 'text_left':
-         $parval = translit_ru($parval);
+   case 'text_left':
+      $parval = translit_ru($parval);
       break;
    }
 }
--- lib/utils-inet.php
+++ lib/utils-inet.php
@@ -6,13 +6,13 @@ avreg_ipv4_cmp($a1, $m1, $a2, $m2)
    if ( !is_int($a1) || !is_int($m1) || !is_int($a2) || !is_int($m2) )
       return FALSE;
 
-    $i1 = 0xFFFFFFFF & $a1 & $m1 & $m2;
-    $i2 = 0xFFFFFFFF & $a2 & $m1 & $m2;
+   $i1 = 0xFFFFFFFF & $a1 & $m1 & $m2;
+   $i2 = 0xFFFFFFFF & $a2 & $m1 & $m2;
 
-    if ( 0 === ($i1 ^ $i2) )
-        return TRUE;
-    else
-        return FALSE;
+   if ( 0 === ($i1 ^ $i2) )
+      return TRUE;
+   else
+      return FALSE;
 } /* avreg_ipv4_cmp() */
 
 
@@ -28,40 +28,40 @@ avreg_inet_network($ip_addr_or_acl=NULL)
       'mask'   =>  NULL,
       'mask_a' => '255.255.255.255');
 
-    if ( 0 === strcasecmp($ip_addr_or_acl, 'localhost') ) {
-        $ret['addr_a'] = '127.0.0.1';
-    }  else if ( 0 === strcasecmp($ip_addr_or_acl, 'any') || $ip_addr_or_acl === '*' ) {
-        $ret['addr_a'] = '0.0.0.0.0'; // INADDR_ANY
-        $ret['mask_a'] = '0.0.0.0.0';
-        $ret['addr'] = 0; // INADDR_ANY
-        $ret['mask'] = 0;
-        return $ret;
-    } else if ( preg_match('%^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$%', $ip_addr_or_acl, $matches) ) {
+   if ( 0 === strcasecmp($ip_addr_or_acl, 'localhost') ) {
+      $ret['addr_a'] = '127.0.0.1';
+   }  else if ( 0 === strcasecmp($ip_addr_or_acl, 'any') || $ip_addr_or_acl === '*' ) {
+      $ret['addr_a'] = '0.0.0.0.0'; // INADDR_ANY
+      $ret['mask_a'] = '0.0.0.0.0';
+      $ret['addr'] = 0; // INADDR_ANY
+      $ret['mask'] = 0;
+      return $ret;
+   } else if ( preg_match('%^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$%', $ip_addr_or_acl, $matches) ) {
       $ret['addr_a'] = $matches[1];
       $ret['mask_a'] = $matches[2];
-    } else if ( preg_match('%^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d{2})$%',
-$ip_addr_or_acl, $matches) ) {
-      $ret['addr_a'] = $matches[1];
-      $ret['mask_a'] = $matches[2];
-      $m = $ret['mask_a'];  settype($m,'int');
-      if ( $m > 32 )
+   } else if ( preg_match('%^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d{2})$%',
+      $ip_addr_or_acl, $matches) ) {
+         $ret['addr_a'] = $matches[1];
+         $ret['mask_a'] = $matches[2];
+         $m = $ret['mask_a'];  settype($m,'int');
+         if ( $m > 32 )
             return FALSE;
-      $m32 = 0;
-      if ( $m == 0 ) {
+         $m32 = 0;
+         if ( $m == 0 ) {
             $m32 = 0;
-      } else {
+         } else {
             for ($i=1; $i<$m; $i++) {
-                $m32 |= 0x80000000;
-                $m32 >>= 1;
+               $m32 |= 0x80000000;
+               $m32 >>= 1;
             }
             $m32 |= 0x80000000;
-      }
-      $ret['mask_a'] = long2ip($m32);
-    } else if ( preg_match('%^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$%',
-$ip_addr_or_acl, $matches) ) {
-      $ret['addr_a'] = $matches[1];
-    } else
-        return FALSE;
+         }
+         $ret['mask_a'] = long2ip($m32);
+      } else if ( preg_match('%^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$%',
+         $ip_addr_or_acl, $matches) ) {
+            $ret['addr_a'] = $matches[1];
+         } else
+            return FALSE;
 
    $ret['addr'] = ip2long($ret['addr_a']);
    $ret['mask'] = ip2long($ret['mask_a']);
@@ -70,6 +70,6 @@ $ip_addr_or_acl, $matches) ) {
       return FALSE;
    else
       return $ret;
-} /* avreg_ipv4_cmp() */
+} /* avreg_inet_network() */
 
 ?>
