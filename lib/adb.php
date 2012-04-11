b3e941516cd99f2a2685d7728afbedfc6ec801ca
ninja git <admin@moonion.com>
2012-04-11 10:55:22 +0300

    suport postgres admin

--- lib/adb.php
+++ lib/adb.php
@@ -226,6 +226,260 @@ class Adb {
 	}
 	
 	
+	public function add_camera ($bind_mac, $cam_nr, $parname, $parval, $host, $user) {
+		$parval = $parval == null ? 'NULL' : "'$parval'";
+		$query = 'INSERT INTO CAMERAS ';
+   		$query .= '(BIND_MAC, CAM_NR, PARNAME, PARVAL, CHANGE_HOST, CHANGE_USER)';
+   		$query .=" VALUES ('".$bind_mac."',".$cam_nr.",'".$parname."',".$parval.",'".$host."','".$user."')";
+		$this->_db->query($query);
+	}
+	
+	public function update_camera ($bind_mac, $cam_nr, $parname, $parval, $host, $user) {
+		$parval = $parval == null ? 'NULL' : "'$parval'";
+		$query = 'UPDATE CAMERAS SET';
+  		$query .= " PARVAL = $parval";
+   		$query .= ", CHANGE_HOST = '$host'";
+   		$query .= ", CHANGE_USER = '$user'";
+   		$query .= " WHERE BIND_MAC = '$bind_mac'";
+   		$query .= " AND CAM_NR = $cam_nr";
+		$query .= " AND PARNAME = '$parname'";
+		$this->_db->query($query);
+	}
+	
+	public function replace_camera ($bind_mac, $cam_nr, $parname, $parval, $host, $user) {
+		$query = 'SELECT * FROM CAMERAS ';
+   		$query .= " WHERE BIND_MAC = '$bind_mac'";
+   		$query .= " AND CAM_NR = $cam_nr";
+  		$query .= " AND PARNAME = '$parname'";
+		$res = $this->_db->query($query);
+		$res->fetchInto($line);
+		if (empty($line)) {
+			$this->add_camera($bind_mac, $cam_nr, $parname, $parval, $host, $user);
+		} else {
+			$this->update_camera($bind_mac, $cam_nr, $parname, $parval, $host, $user);
+		}
+	}
+	
+	
+	public function get_def_cam_params($cam_nr = 0, $bind_mac = 'local') {
+		$cams = array();
+		$query = 'SELECT CAM_NR, PARNAME, PARVAL, CHANGE_HOST, CHANGE_USER, CHANGE_TIME FROM CAMERAS';
+        $query .= ' WHERE BIND_MAC=\''.$bind_mac.'\' AND (CAM_NR=0 OR CAM_NR='.$cam_nr.')';
+		 
+		$res = $this->_db->query($query);
+    	while ($res->fetchInto($line, DB_FETCHMODE_ASSOC)) {
+    		    		
+    		
+    		$cams[] = array(
+    			'CAM_NR' => trim($line[$this->_key('CAM_NR')]),
+   				'PARAM' => trim($line[$this->_key('PARNAME')]),
+				'VALUE' => trim($line[$this->_key('PARVAL')]),
+				'CHANGE_HOST' => trim($line[$this->_key('CHANGE_HOST')]),
+				'CHANGE_USER' => trim($line[$this->_key('CHANGE_USER')]),
+				'CHANGE_TIME' => trim($line[$this->_key('CHANGE_TIME')]),
+    		
+    		);
+    	}
+		return  $cams;
+	}
+	
+	
+	public function get_cam_params($cams_list = '', $param_list = '', $bind_mac = 'local') {
+		$cams = array();
+		$query = 'SELECT CAM_NR, PARNAME, PARVAL FROM CAMERAS';
+        $query .= ' WHERE BIND_MAC=\''.$bind_mac.'\' AND (CAM_NR=0 ';
+        if (!empty($cams_list)) {
+        	 $query .= ' OR CAM_NR in('.$cams_list.')';
+        }
+        $query .= ')';
+        $query .= ' AND PARNAME IN ('.$param_list.') AND  PARVAL<>\'\' AND PARVAL IS NOT NULL ';
+   		$query .= ' ORDER BY CAM_NR';
+		$res = $this->_db->query($query);
+    	while ($res->fetchInto($line, DB_FETCHMODE_ASSOC)) {
+    		$cams[] = array(
+    			'CAM_NR' => trim($line[$this->_key('CAM_NR')]),
+   				'PARAM' => trim($line[$this->_key('PARNAME')]),
+				'VALUE' => trim($line[$this->_key('PARVAL')]),
+    		);
+    	}
+		return  $cams;
+	}
+	
+	public function max_cam_nr($bind_mac = 'local') {
+		$query = 'SELECT MAX(CAM_NR) AS LAST_NUM FROM CAMERAS WHERE BIND_MAC=\''.$bind_mac.'\'';
+		$res = $this->_db->query($query);
+		$res->fetchInto($line);
+		return isset($line[0]) ? $line[0] : false; 
+	}	
+	public function delete_camera($cam_nr, $bind_mac = 'local') {
+	    $query = sprintf('DELETE FROM CAMERAS WHERE BIND_MAC=\''.$bind_mac.'\' AND CAM_NR=%d', $cam_nr);
+		$this->_db->query($query);
+	}
+	
+	
+	
+	public function add_monitors($display,$mon_nr,$mon_type,$mon_name, $remote_addr, $login_user, $fWINS, $vWINS,$bind_mac = 'local') {
+		$query = sprintf('INSERT INTO MONITORS (BIND_MAC, DISPLAY, MON_NR, MON_TYPE, MON_NAME, %s, CHANGE_HOST, CHANGE_USER) VALUES (\'local\', \'%s\', %d, \'%s\', \'%s\', %s, \'%s\', \'%s\')',
+		implode (', ',$fWINS), $display, $mon_nr, $mon_type, $mon_name, implode (', ',$vWINS), $remote_addr, $login_user);
+		$this->_db->query($query);
+	}
+	public function delete_monitors($display, $mon_nr, $bind_mac = 'local') {
+		$query = 'DELETE FROM MONITORS';
+		$query .= " WHERE BIND_MAC ='$bind_mac'";
+		$query .= " AND DISPLAY ='$display'";
+		$query .= " AND MON_NR = $mon_nr";		
+		$this->_db->query($query);
+	}
+			
+ 	 
+	public function update_monitors ($display,$mon_nr,$mon_type,$mon_name, $host, $user, $fWINS, $vWINS,$bind_mac = 'local') {
+		$query = 'UPDATE CAMERAS SET ';
+   		$query .= "MON_TYPE = '$mon_type'";
+   		$query .= ", MON_NAME = '$mon_name;";
+   		$query .= ", CHANGE_HOST = '$host'";
+   		$query .= ", CHANGE_USER = '$user'";
+   		
+   		for ($i = 0; $i < count($vWINS); $i++) {
+   			$query .= ", {$fWINS[$i]} = {$vWINS[$i]}";
+   		}
+   		   		
+   		
+   		$query .= " WHERE BIND_MAC ='$bind_mac'";
+		$query .= " AND DISPLAY ='$display'";
+		$query .= " AND MON_NR = $mon_nr";		
+   		
+		$this->_db->query($query);
+	}
+	
+	public function replace_monitors ($fWINS,$display,$mon_nr,$mon_type,$mon_name, $host, $user, $fWINS, $vWINS, $bind_mac = 'local') {
+		$query = 'SELECT * FROM MONITORS ';
+   		$query .= " WHERE BIND_MAC = '$bind_mac'";
+   		$query .= " AND MON_NR = $mon_nr";
+  		$query .= " AND DISPLAY = '$display'";
+		$res = $this->_db->query($query);
+		$res->fetchInto($line);
+		if (empty($line)) {
+			$this->add_monitors($display,$mon_nr,$mon_type,$mon_name, $host, $user, $fWINS, $vWINS);
+		} else {
+			$this->update_monitors($display,$mon_nr,$mon_type,$mon_name, $host, $user, $fWINS, $vWINS);
+		}
+	}	
+
+	public function get_monitor($display, $mon_nr, $bind_mac = 'local') {
+		 $query = 'SELECT MON_NR, MON_TYPE, MON_NAME, IS_DEFAULT, ' .
+         'WIN1, WIN2, WIN3, WIN4, WIN5, WIN6, WIN7, WIN8, WIN9, WIN10, WIN11, WIN12, WIN13, WIN14, WIN15, WIN16, WIN17, WIN18, WIN19, WIN20, WIN21, WIN22, WIN23, WIN24, WIN25, '.
+         'CHANGE_HOST, CHANGE_USER, CHANGE_TIME '.
+         'FROM MONITORS '.
+         'WHERE BIND_MAC=\''.$bind_mac.'\' AND DISPLAY=\''.$display.'\' AND MON_NR='.$mon_nr;
+		 $res = $this->_db->query($query);
+		 $res->fetchInto($line);
+		 return $line;
+	}
+	   
+	
+	
+	public function get_monitors($bind_mac = 'local'){
+		$mon = array();
+		$query = "SELECT * FROM MONITORS";
+		$query .= " WHERE BIND_MAC='$bind_mac'";
+		$query .= " ORDER BY MON_NR";
+		
+   		$res = $this->_db->query($query);
+    	while ($res->fetchInto($line, DB_FETCHMODE_ASSOC)) {
+    		$m = array();
+    		foreach ($line as $k=>$v) {
+    			$k = strtoupper($k);
+    			$m[$k] = trim($v);
+    		}
+    		
+    		$mon[] = $m;
+    	}
+    		
+		return  $mon;
+	}
+   
+	public function add_user($u_host, $u_name, $passwd_f, $groups, $u_devacl, $u_forced_saving_limit, $sessions_per_cam,$limit_fps,$nonmotion_fps, $limit_kbps, $session_time, $session_volume, $u_longname, $remote_addr, $login_user) {
+		$query = sprintf('INSERT INTO USERS 
+         ( ALLOW_FROM, USER_LOGIN, PASSWD, STATUS, ALLOW_CAMS, FORCED_SAVING_LIMIT, SESSIONS_PER_CAM,
+         LIMIT_FPS, NONMOTION_FPS, LIMIT_KBPS,
+         SESSION_TIME, SESSION_VOLUME,
+         LONGNAME, CHANGE_HOST, CHANGE_USER, CHANGE_TIME) 
+         VALUES ( %s, %s, %s %u, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NOW())',
+            sql_format_str_val($u_host),
+            sql_format_str_val($u_name),
+            $passwd_f,
+            $groups,
+            sql_format_str_val($u_devacl),
+            sql_format_int_val($u_forced_saving_limit),
+            sql_format_int_val($sessions_per_cam),
+            sql_format_str_val($limit_fps),
+            sql_format_str_val($nonmotion_fps),
+            sql_format_int_val($limit_kbps),
+            sql_format_int_val($session_time),
+            sql_format_int_val($session_volume),
+            sql_format_str_val($u_longname),
+            sql_format_str_val($remote_addr),
+            sql_format_str_val($login_user));
+            
+      $this->_db->query($query);   
+	}
+	
+	 public function update_user($u_host,$u_name,$passwd_changed, $groups, $u_devacl, $u_forced_saving_limit, $sessions_per_cam, $limit_fps, $nonmotion_fps, $limit_kbps, $session_time, $session_volume, $u_longname, $remote_addr, $login_user, $old_u_host,$old_u_name){
+	 	$query = sprintf(
+         'UPDATE USERS SET ALLOW_FROM=%s, USER_LOGIN=%s, %s STATUS=%d, ALLOW_CAMS=%s, FORCED_SAVING_LIMIT=%s, SESSIONS_PER_CAM=%s, LIMIT_FPS=%s, NONMOTION_FPS=%s, LIMIT_KBPS=%s, SESSION_TIME=%s, SESSION_VOLUME=%s, LONGNAME=%s, CHANGE_HOST=%s, CHANGE_USER=%s, CHANGE_TIME=NOW() WHERE HOST=%s AND USER=%s',
+         sql_format_str_val($u_host),
+         sql_format_str_val($u_name),
+         $passwd_changed,
+         $groups,
+         sql_format_str_val($u_devacl),
+         sql_format_int_val($u_forced_saving_limit),
+         sql_format_int_val($sessions_per_cam),
+         sql_format_str_val($limit_fps),
+         sql_format_str_val($nonmotion_fps),
+         sql_format_int_val($limit_kbps),
+         sql_format_int_val($session_time),
+         sql_format_int_val($session_volume),
+         sql_format_str_val($u_longname),
+         sql_format_str_val($remote_addr),
+         sql_format_str_val($login_user),
+         sql_format_str_val($old_u_host),
+         sql_format_str_val($old_u_name));
+         $this->_db->query($query);   
+	 }
+      
+	
+	
+	public function delete_user($u_name, $u_host, $u_status){
+		$query = sprintf('DELETE FROM USERS WHERE USER_LOGIN="%s" AND ALLOW_FROM="%s" AND STATUS=%u',
+			$u_name, $u_host, $u_status);
+		 $this->_db->query($query);   
+	}
+	
+	public function get_users($status) {
+		$users = array();
+		$query = 'SELECT ALLOW_FROM, USER_LOGIN, LONGNAME, CHANGE_HOST, CHANGE_USER, CHANGE_TIME '.
+				 'FROM USERS '.
+				 "WHERE STATUS = $status ".
+				 'ORDER BY ALLOW_FROM, USER_LOGIN';
+		$res = $this->_db->query($query);
+    	while ($res->fetchInto($line, DB_FETCHMODE_ASSOC)) {
+    		
+    		$users[] =array(
+    			'HOST' => trim($line[$this->_key('ALLOW_FROM')]),
+    		    'USER' => trim($line[$this->_key('USER_LOGIN')]),
+    			'LONGNAME' => trim($line[$this->_key('LONGNAME')]),
+    			'CHANGE_HOST' => trim($line[$this->_key('CHANGE_HOST')]),
+    			'CHANGE_USER' => trim($line[$this->_key('CHANGE_USER')]),
+    			'CHANGE_TIME' => trim($line[$this->_key('CHANGE_TIME')]),
+    		
+    		);
+    	}
+    	return $users;
+	}
+	
+	
+	
+	
 	private function _key($str){
 		if ($this->_dbtype == 'pgsql') {
 			return strtolower($str);
@@ -272,7 +526,7 @@ class Adb {
 		
 		return $str;
 	}
-	public function _date_format($value) {
+	private function _date_format($value) {
 		if ($this->_dbtype == 'pgsql') {
 			$str = "to_char(%%, 'yyyy_mm_dd_hh24')";
 		} else {
@@ -284,7 +538,7 @@ class Adb {
 		
 	}
 
-	public function _timediff($d1, $d2){
+	private function _timediff($d1, $d2){
 		if ($this->_dbtype == 'pgsql') {
 			$str = $d1."-".$d2;
 		} else {
