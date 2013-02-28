1c6eeda61717cfb2a0d808c348465cd060577838
Serhey Doniy <sergey.doniy@moonion.com>
2013-02-28 13:30:51 +0200

    Относительные пути  и раскладки (исправленo user_layouts.setCookie();

--- online/view.js
+++ online/view.js
@@ -147,7 +147,7 @@ function img_click(clicked_div) {
 	
    var tmp_div;
    var clicked_div_jq = $(clicked_div);
-   var win_geo; 
+   var win_geo;
    var i;
    //номер ячейки
    var win_nr = parseInt(($(clicked_div).attr('id')).match(/\d+/gi));
@@ -252,7 +252,7 @@ function img_click(clicked_div) {
       set_win_header_size(clicked_div_jq, NAME_DIV_H);
 
       win_geo = new calc_win_geo(CANVAS_W, CANVAS_H, CamsAspectRatio, 1, 1, 1);
-
+      alert(win_geo.cam_h +'\n'+win_geo.com_w);
       clicked_div_jq.css('top',  calc_win_top (win_geo, 0));
       clicked_div_jq.css('left', calc_win_left(win_geo, 0));
   
@@ -1341,7 +1341,7 @@ function canvas_growth() {
    		var url = '../admin/web_mon_list.php';
 	   	if(user_layouts.isLocalStorageAvailable()){
            if(user_layouts.client_layouts_json==undefined){
-                   this.setCookie('layouts', '', -1, '/', window.location.hostname, '');
+                   user_layouts.setCookie('layouts', '', -1, '/', window.location.hostname, '');
            }
    			else if(user_layouts.client_layouts_json){
                 var lay_user = JSON.stringify(user_layouts.client_layouts);
