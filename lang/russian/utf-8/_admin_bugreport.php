56e3c4a9bdd298ece60c16531b19a6eea58470b2
Andrey Nikitin <avreg-support@mail.ru>
2013-03-20 13:45:02 +0400

    update bugreport page (3)

--- lang/russian/utf-8/_admin_bugreport.php
+++ lang/russian/utf-8/_admin_bugreport.php
@@ -11,7 +11,6 @@ $bugs_rules = <<<EOD
 Правила и рекомендуемая последовательность действий:
 <ol>
 <li>Пользоваться этой страницей нужно только с клиента (хоста) с которого посредcтвом <a href="http://avreg.net/manual_tuning_avreg-site.html#sudo" target="_blanc">настроек sudo</a> позволятется управлять демоном avregd на <a href="/admin/control.php">странице Управление</a>.
-<li>Если вы используете video4linux устройства видеозахвата (PCI-платы или USB-камеры), установите пакет v4l-utils.</li>
 <li>Получите архив с лог-файлами и настройками AVReg (кнопка ниже) как можно скорее с момента возникновения ошибки.</li>
 <li>Отправьте полученный архив лог-файлов с указанием номера камеры, описанием ошибки и времени её возникновения на e-mail <a href="mailto:avreg-support@mail.ru?subject=bugreport" title="AVReg bug report">avreg-support@mail.ru</a></li>
 </ol>
