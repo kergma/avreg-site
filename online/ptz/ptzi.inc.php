28a296165deeca2b740fe70c0b2b84499b8d36d4
Sergey Pushkin <pushkinsv@gmail.com>
2014-02-28 11:41:45 +0800

    Forgotten file

---
+++ online/ptz/ptzi.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file online/ptz/ptzi.inc.php
+ * @brief Base class for camera PTZ interface
+ */
+class PTZi
+{
+	var $camurl;
+	function get_bounds()
+	{
+		return array();
+	}
+	function get_pos()
+	{
+		return array();
+	}
+
+};
+
+?>
